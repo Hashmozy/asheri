ee6e7ecc41fadf48338f315a5656d70301dedd2d
musatallen <musa@tallenhub.com>
2025-11-27 13:55:45 +0300

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/asheri" : "",
+  assetPrefix: isProd ? "/asheri/" : "",
   typescript: {
     ignoreBuildErrors: true,
   },
